9e69d3d51d3fe91ac58e9a121930712b618f2c10
Mohammed Islam <mmislam101@gmail.com>
2020-05-09 13:50:41 -0700

    Update bucket policies to be static host

--- index.js
+++ index.js
@@ -3,8 +3,37 @@ const aws = require("@pulumi/aws");
 const pulumi = require("@pulumi/pulumi");
 const mime = require("mime");
 
+// Create an S3 Bucket Policy to allow public read of all objects in bucket
+// This reusable function can be pulled out into its own module
+function publicReadPolicyForBucket(bucketName) {
+    return JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [{
+        Effect: "Allow",
+        Principal: "*",
+        Action: [
+            "s3:GetObject"
+        ],
+        Resource: [
+            `arn:aws:s3:::${bucketName}/*` // policy refers to bucket name explicitly
+        ]
+        }]
+    })
+}
+
 // Create an S3 bucket
-let siteBucket = new aws.s3.Bucket("admin.khatmapp.com");
+let siteBucket = new aws.s3.Bucket("admin.khatmapp.com", {
+    website: {
+      indexDocument: "index.html",
+      errorDocument: "index.html", // Required for SPA
+    },
+});
+
+// Set the access policy for the bucket so all objects are readable
+let bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
+    bucket: siteBucket.bucket,
+    policy: siteBucket.bucket.apply(publicReadPolicyForBucket)
+});
 
 let siteDir = "www"; // directory for content files
 
@@ -19,3 +48,4 @@ for (let item of require("fs").readdirSync(siteDir)) {
 }
 
 exports.bucketName = siteBucket.bucket; // create a stack export for bucket name
+exports.websiteUrl = siteBucket.websiteEndpoint; // output the endpoint as a stack output
