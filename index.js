fbb92081155decf912bd0bc8f239b536c56dc89c
Mohammed Islam <mmislam101@gmail.com>
2020-05-12 16:29:38 -0700

    Add cloudwatch dashboard

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 "use strict";
+const aws = require("@pulumi/aws");
 const pulumi = require("@pulumi/pulumi");
 const {createStaticSPASite} = require("./lib/static_site.js");
 const {createECS} = require("./lib/ecs.js");
 const {createVPC} = require("./lib/vpc.js");
+const {createCloudWatchDashboard} = require("./lib/cloudwatch.js");
 
 
-createStaticSPASite("admin.khatmapp.com");
-const vpc = createVPC("khatm-app")
-const listener = createECS(vpc);
+// createStaticSPASite("admin.khatmapp.com"); // TODO: Disable this for now. Causing problems when updating
+const vpc = createVPC("khatm-app");
+const {listener, service, cluster} = createECS(vpc);
+const dashboardName = "khatm-api";
+
+const services = {
+    ecs: {service, cluster}
+}
+createCloudWatchDashboard(dashboardName, services);
 
 exports.frontendURL = pulumi.interpolate `http://${listener.endpoint.hostname}/`;
+exports.dashboardUrl =
+    `https://${aws.config.region}.console.aws.amazon.com/cloudwatch/home?` +
+        `region=${aws.config.region}#dashboards:name=${dashboardName}`;
