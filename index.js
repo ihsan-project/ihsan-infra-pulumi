59f2471ebd54b637280609e2f443af11828982a7
Mohammed Islam <mmislam101@gmail.com>
2020-05-11 17:58:57 -0700

    Contain everything inside a vpc

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const pulumi = require("@pulumi/pulumi");
 const {createStaticSPASite} = require("./lib/static_site.js");
 const {createECS} = require("./lib/ecs.js");
+const {createVPC} = require("./lib/vpc.js");
 
 
 createStaticSPASite("admin.khatmapp.com");
-const listener = createECS();
+const vpc = createVPC("khatm-app")
+const listener = createECS(vpc);
 
 exports.frontendURL = pulumi.interpolate `http://${listener.endpoint.hostname}/`;
