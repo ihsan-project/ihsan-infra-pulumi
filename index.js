726c83cc47900a501ec06e9f6efe78733682e543
Mohammed Islam <mmislam101@gmail.com>
2020-08-01 23:17:46 -0700

    Cleanup

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ const {createPipeline} = require("./lib/code_pipeline.js");
 const {createCloudWatchDashboard} = require("./lib/cloudwatch.js");
 
 // createStaticSPASite("admin.khatmapp.com"); // TODO: Disable this for now. Causing problems when updating
-// const security = createVPC("khatm-app");
+const security = createVPC("khatm-app");
 const roles = createRoles();
-// const db = createRDS(security);
-// const {listener, service, cluster} = createECS(security, db, roles);
+const db = createRDS(security);
+const {listener, service, cluster} = createECS(security, db, roles);
 
-// recordCNAME("api", listener.endpoint.hostname); // Creates api.khatmapp.com CNAME entry in DNS
+recordCNAME("api", listener.endpoint.hostname); // Creates api.khatmapp.com CNAME entry in DNS
 
-// const services = {
-//     db,
-//     ecs: {service, cluster}
-// }
-// const dashboardName = "khatm-api";
-// createCloudWatchDashboard(dashboardName, services);
+const services = {
+    db,
+    ecs: {service, cluster}
+}
+const dashboardName = "khatm-api";
+createCloudWatchDashboard(dashboardName, services);
 
 createPipeline(roles);
 
-// exports.lbURL = pulumi.interpolate `http://${listener.endpoint.hostname}/`;
-// exports.dashboardUrl =
-//     `https://${aws.config.region}.console.aws.amazon.com/cloudwatch/home?` +
-//         `region=${aws.config.region}#dashboards:name=${dashboardName}`;
+exports.lbURL = pulumi.interpolate `http://${listener.endpoint.hostname}/`;
+exports.dashboardUrl =
+    `https://${aws.config.region}.console.aws.amazon.com/cloudwatch/home?` +
+        `region=${aws.config.region}#dashboards:name=${dashboardName}`;
