4ea55322a856cf116d97dd1d0ffefff65545458d
Mohammed Islam <mmislam101@gmail.com>
2020-08-08 14:22:57 -0700

    Move rds into environment setup

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const pulumi = require("@pulumi/pulumi");
 const {createStaticSPASite} = require("./lib/static_site.js");
 const {recordCNAME} = require("./lib/dns.js");
 const {createECS} = require("./lib/ecs.js");
-const {createVPC} = require("./lib/vpc.js");
+const {createEnvironment} = require("./lib/vpc.js");
 const {createRDS} = require("./lib/rds.js");
 const {createRoles} = require("./lib/roles.js");
 const {createPipeline} = require("./lib/code_pipeline.js");
@@ -12,24 +12,25 @@ const {createCloudWatchDashboard} = require("./lib/cloudwatch.js");
 
 const appName = process.env.APP_NAME || 'khatm';
 
-// createStaticSPASite("admin.khatmapp.com"); // TODO: Disable this for now. Causing problems when updating
-const security = createVPC(appName);
+// Setup Foundations
+const environment = createEnvironment(appName);
+recordCNAME("api", environment.alb.albListener.endpoint.hostname); // Create api.hostname.com CNAME entry in DNS
+const db = createRDS(appName, environment);
+
+// Setup Apps
 const roles = createRoles(appName);
-const db = createRDS(appName, security);
-const {listener, service, cluster} = createECS(appName, security, db, roles);
-
-// Create api.hostname.com CNAME entry in DNS
-recordCNAME("api", listener.endpoint.hostname);
-
-const services = {
+const {service, cluster} = createECS(appName, environment, db, roles);
+createCloudWatchDashboard(appName, {
     db,
     ecs: {service, cluster}
-}
-createCloudWatchDashboard(appName, services);
+});
 
 createPipeline(appName, roles);
 
-exports.lbURL = pulumi.interpolate `http://${listener.endpoint.hostname}/`;
+// TODO: Disable this for now. Causing problems when updating
+// createStaticSPASite("admin.khatmapp.com");
+
+exports.lbURL = pulumi.interpolate `http://${environment.alb.albListener.endpoint.hostname}/`;
 exports.dashboardUrl =
     `https://${aws.config.region}.console.aws.amazon.com/cloudwatch/home?` +
         `region=${aws.config.region}#dashboards:name=${appName}`;
