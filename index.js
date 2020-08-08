cfad901ec6a7d97b495b16f0539badb7148d8c4b
Mohammed Islam <mmislam101@gmail.com>
2020-08-08 14:59:07 -0700

    Break roles apart and set them for who needs them

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const {recordCNAME} = require("./lib/dns.js");
 const {createECS} = require("./lib/ecs.js");
 const {createEnvironment} = require("./lib/vpc.js");
 const {createRDS} = require("./lib/rds.js");
-const {createRoles} = require("./lib/roles.js");
 const {createPipeline} = require("./lib/code_pipeline.js");
 const {createCloudWatchDashboard} = require("./lib/cloudwatch.js");
 
@@ -16,16 +15,19 @@ const appName = process.env.APP_NAME || 'khatm';
 const environment = createEnvironment(appName);
 recordCNAME("api", environment.alb.albListener.endpoint.hostname); // Create api.hostname.com CNAME entry in DNS
 const db = createRDS(appName, environment);
+createPipeline(appName);
+
+// TODO: Dependencies cause the Foundations to be setup before the following can be setup
+//       Sure we can use dependencies, but we need to seperate these concerns anways into two different pulumi apps:
+//       https://github.com/khatm-org/khatm-infrastructure/issues/27
 
 // Setup Apps
-const roles = createRoles(appName);
-const {service, cluster} = createECS(appName, environment, db, roles);
+const {service, cluster} = createECS(appName, environment, db);
 createCloudWatchDashboard(appName, {
     db,
     ecs: {service, cluster}
 });
 
-createPipeline(appName, roles);
 
 // TODO: Disable this for now. Causing problems when updating
 // createStaticSPASite("admin.khatmapp.com");
